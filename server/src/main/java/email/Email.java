a63bfde5b82b1e4ba997329b2501b48dfd1dea71
b9025633 <b9025633@newcastle.ac.uk>
2020-12-08 18:37:03 +0000

    Added email recipient tests

--- server/src/main/java/email/Email.java
+++ server/src/main/java/email/Email.java
@@ -1,4 +1,15 @@
 package email;
 
+import java.util.HashSet;
+
 public class Email {
+
+
+	public void addRecipient(String email) {
+
+	}
+
+	public HashSet<String> getRecipients() {
+		return new HashSet<String>();
+	}
 }
--- server/src/test/java/email/EmailTest.java
+++ server/src/test/java/email/EmailTest.java
@@ -1,7 +1,44 @@
 package email;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmailTest {
 
+	private Email email;
+
+	@BeforeEach
+	void setup() {
+		email = new Email();
+	}
+
+	@Test
+	void testAddRecipientToEmail() {
+		String recipient = "quicksolutions.doorbell@gmail.com";
+		email.addRecipient(recipient);
+
+		assertTrue(email.getRecipients().contains(recipient));
+	}
+
+	@Test
+	void testAddMultipleRecipientsToEmail() {
+		String recipient1 = "quicksolutions.doorbell@gmail.com";
+		String recipient2 = "quicksolutions.doorbell2@gmail.com";
+		email.addRecipient(recipient1);
+		email.addRecipient(recipient2);
+
+		assertTrue(email.getRecipients().contains(recipient1) &&
+				email.getRecipients().contains(recipient2));
+	}
+
+	@Test
+	void testAddDuplicateRecipientToEmail() {
+		String recipient = "quicksolutions.doorbell@gmail.com";
+		email.addRecipient(recipient);
+		email.addRecipient(recipient);
+		email.addRecipient(recipient);
+		assertEquals(email.getRecipients().size(), 1);
+	}
 }
