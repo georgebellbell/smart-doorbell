b6eb9c14a9c8e5e8bd699d4d96513c873bc3d053
b9025633 <b9025633@newcastle.ac.uk>
2020-12-09 18:27:47 +0000

    Added implementation of generating code for TwoFactorAuthentication

--- server/src/main/java/authentication/TwoFactorAuthentication.java
+++ server/src/main/java/authentication/TwoFactorAuthentication.java
@@ -1,5 +1,7 @@
 package authentication;
 
+import java.security.SecureRandom;
+
 public class TwoFactorAuthentication {
 
 	private final String email;
@@ -12,7 +14,19 @@ public class TwoFactorAuthentication {
 		return email;
 	}
 
+	/**
+	 * Generates a 6 digit code that is securely random
+	 * @return generated 6 digit code
+	 */
 	public String generateCode() {
-		return "";
+		SecureRandom secureRandom = new SecureRandom();
+		StringBuilder generatedCode = new StringBuilder();
+
+		// Generate 6 random numbers and add them to generated code
+		for (int i=0; i < 6; i++) {
+			generatedCode.append(secureRandom.nextInt(10));
+		}
+
+		return generatedCode.toString();
 	}
 }
