e1f0b914b2c4bc023b29a74b20e07144bfb01d71
b9025633 <b9025633@newcastle.ac.uk>
2020-12-09 17:44:39 +0000

    Added test for email being set on initialisation of TwoFactorAuthentication class

--- server/src/main/java/authentication/TwoFactorAuthentication.java
+++ server/src/main/java/authentication/TwoFactorAuthentication.java
@@ -1,4 +1,12 @@
 package authentication;
 
 public class TwoFactorAuthentication {
+
+	public TwoFactorAuthentication(String email) {
+
+	}
+
+	public String getUserEmail() {
+		return "";
+	}
 }
--- server/src/test/java/authentication/TwoFactorAuthenticationTest.java
+++ server/src/test/java/authentication/TwoFactorAuthenticationTest.java
@@ -1,7 +1,24 @@
 package authentication;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TwoFactorAuthenticationTest {
+
+	private TwoFactorAuthentication twoFactor;
+	private String userEmail;
+
+	@BeforeEach
+	void setup() {
+		userEmail = "quicksolutions.doorbell@gmail.com";
+		twoFactor = new TwoFactorAuthentication(userEmail);
+	}
+
+	@Test
+	void testEmailSetOnInitialisation() {
+		assertEquals(userEmail, twoFactor.getUserEmail());
+	}
 
 }
