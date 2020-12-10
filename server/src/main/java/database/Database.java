2ecc71165485fb853f54ac8789b5698ed4e7afb4
Dominykas Makarovas <b9021925@newcastle.ac.uk>
2020-12-10 16:45:44 +0000

    Added basic CRUD functionality to database class, experimenting the connection with the database.

--- server/src/main/java/database/Database.java
+++ server/src/main/java/database/Database.java
@@ -2,51 +2,108 @@ package database;
 
 import com.jcraft.jsch.*;
 
-import java.io.OutputStream;
 import java.sql.*;
-import java.util.Properties;
 
 public class Database {
-	public static void main(String[] args) {
-		int lport = 3306;
-		String host = "linux.cs.ncl.ac.uk";
-		String rhost = "cs-db.ncl.ac.uk";
-		int rport = 3306;
-		String user = "b9021925";
-		String dbuserName = "t2033t17";
-		String dbpassword = "KnewBut+(Fin";
-		String dburl = "jdbc:mysql://localhost:3306/t2033t17";
-		String driverName = "com.mysql.cj.jdbc.Driver";
+	// SSH connection info
+	private final String HOST = "linux.cs.ncl.ac.uk";
+	private final String RHOST = "cs-db.ncl.ac.uk";
+	private final int PORT = 3306;
+	private final String USER = "b9021925";
 
-		Connection conn;
-		Session session;
-		Statement stmt;
-		int assigned_port = -1;
+	// Database connection info
+	private final String DBUSERNAME = "t2033t17";
+	private final String DBPASSWORD = "KnewBut+(Fin";
+	private final String DBURL = "jdbc:mysql://localhost:3306/t2033t17";
+	private final String DRIVERNAME = "com.mysql.cj.jdbc.Driver";
 
+	Connection conn;
+	Session session;
+	PreparedStatement statement;
+	int assigned_port = -1;
+	JSch jsch = new JSch();
+
+	public Database() {
+		connectToDatabase();
+	}
+	public void establishSSH() {
 		try {
-			JSch jsch = new JSch();
 			java.util.Properties config = new java.util.Properties();
 			config.put("StrictHostKeyChecking", "no");
-			session = jsch.getSession(user, host, 22);
-			session.setPassword("unipassword");
+			session = jsch.getSession(USER, HOST, 22);
+			session.setPassword("");
 			session.setConfig(config);
 			session.connect();
-			assigned_port = session.setPortForwardingL(lport, rhost, rport);
-			System.out.println("Connected");
-
-			Class.forName(driverName).newInstance();
-			conn = DriverManager.getConnection(dburl, dbuserName, dbpassword);
-			stmt = conn.createStatement();
-			System.out.println ("Database connection established");
-			ResultSet rs = stmt.executeQuery("SELECT * FROM accounts");
-			while (rs.next()) {
-				System.out.println(rs.getString("Username"));
-			}
-			stmt.close();
-			conn.close();
-			System.out.println("Connection closed");
+			assigned_port = session.setPortForwardingL(PORT, RHOST, PORT);
+			System.out.println("SSH Connected");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	public boolean connectToDatabase() {
+		establishSSH();
+		try {
+			Class.forName(DRIVERNAME).newInstance();
+			conn = DriverManager.getConnection(DBURL, DBUSERNAME, DBPASSWORD);
+			System.out.println ("Database connection established");
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public boolean addUser(String username, String email, String password, String salt, String role) {
+		try {
+			String query = "INSERT INTO accounts (Username, Email, Password, Salt, Role, Created_at)"
+					+ " VALUES (?, ?, ?, ?, ?, ?)";
+			statement = conn.prepareStatement(query);
+			statement.setString(1, username);
+			statement.setString(2,email);
+			statement.setString(3, password);
+			statement.setString(4, salt);
+			statement.setString(5, role);
+			statement.setTimestamp(6, java.sql.Timestamp.from(java.time.Instant.now()));
+			statement.execute();
+			conn.close();
+			return true;
+		} catch (Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public String getUser(String username) {
+		String email = null;
+		String role = null;
+		String created_at = null;
+		String password = null;
+		String salt = null;
+		try {
+			String query = "SELECT Username, Email, Password, Salt, Role, Created_at  FROM accounts WHERE Username=?";
+			statement = conn.prepareStatement(query);
+			statement.setString(1, username);
+			ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				email = resultSet.getString("Email");
+				password = resultSet.getString("Password");
+				salt = resultSet.getString("Salt");
+				role = resultSet.getString("Role");
+				created_at = resultSet.getString("Created_at");
+			}
+			return username + " " + email + " " + password + " " + salt + " " + role + " " + created_at;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public static void main(String[] args) {
+		Database t17 = new Database();
+		System.out.println(t17.addUser(
+				"Jeff",
+				"John@gmail.com",
+				"Password",
+				"Salt",
+				"admin")
+		);
+		System.out.println(t17.getUser("Dom"));
 	}
 }
