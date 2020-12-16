4046c816082e446012d583a844d2e2a0798558df
Darkwood691 <daleofdarlo@gmail.com>
2020-12-16 10:08:44 +0000

    Adjusted timings of camera capture

--- Camera/main.py
+++ Camera/main.py
@@ -2,16 +2,17 @@ from gpiozero import *
 from time import sleep
 from picamera import PiCamera
 
-
 # GPIO pin setup
 led1 = LED(10)
 button1 = Button(15)
 led2 = LED(27)
 led2.on()
 
-#button2 = Button(3)
 
-def Capture(location = "/home/pi/Desktop/image.jpg"):
+# button2 = Button(3)
+
+# Take picture
+def capture(location="/home/pi/Desktop/image.jpg"):
     camera = PiCamera()
     camera.capture(location)
     camera.close()
@@ -21,16 +22,8 @@ def Capture(location = "/home/pi/Desktop/image.jpg"):
 while True:
     if button1.is_pressed:
         led1.on()
-        Capture()
-        sleep(1)
+        sleep(0.5)
+        capture()
+        sleep(0.5)
     else:
         led1.off()
-
-
-
-
-
-
-
-
-
