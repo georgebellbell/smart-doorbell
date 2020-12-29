5de4f7d2da219d4bf9b85e33c1f07d807b78c336
Darkwood691 <daleofdarlo@gmail.com>
2020-12-29 16:32:20 +0000

    Changed photo filepath to a relative path

--- Camera/main.py
+++ Camera/main.py
@@ -1,6 +1,12 @@
 from gpiozero import *
 from time import sleep
-from picamera import PiCamera
+from picamera import PiCamera  # This library installed on the raspberry pi by default
+import os
+
+
+# Setup file path for image location
+cwd = os.getcwd()
+imagePath = cwd + "/photo.jpg"
 
 # GPIO pin setup
 led1 = LED(10)
@@ -10,22 +16,23 @@ led2.on()
 
 button2 = Button(3)
 
+
 # Take picture
-def capture(location="/home/pi/Desktop/image.jpg"):
-    camera = PiCamera()
-    camera.capture(location)
-    camera.close()
+def capture(location=imagePath):
+	camera = PiCamera()
+	camera.capture(location)
+	camera.close()
 
 
 # Main loop
 while True:
-    if button1.is_pressed:
-        led1.on()
-        sleep(0.5)
-        capture()
-        sleep(0.5)
-    else:
-        led1.off()
+	if button1.is_pressed:
+		led1.on()
+		sleep(0.5)
+		capture()
+		sleep(0.5)
+	else:
+		led1.off()
 
-    if button2.is_pressed:
-        exit()
+	if button2.is_pressed:
+		exit()
