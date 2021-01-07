a95da9584b398a8ad76fac03af7c916c917491fb
b9025633 <b9025633@newcastle.ac.uk>
2021-01-07 22:40:36 +0000

    Added setting of admin user in AdminProtocol

--- server/src/main/java/server/protocol/AdminProtocol.java
+++ server/src/main/java/server/protocol/AdminProtocol.java
@@ -5,9 +5,18 @@ import database.User;
 import server.ResponseHandler;
 
 public class AdminProtocol extends Protocol {
+	private User user;
 
 	public AdminProtocol() {
 		requestResponse.put("login", new ResponseHandler(this::login, "username", "password"));
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public User getUser() {
+		return user;
 	}
 
 	public void login() {
@@ -24,10 +33,14 @@ public class AdminProtocol extends Protocol {
 			// Successful login response
 			response.put("response", "success");
 
+			// Set current user
+			setUser(currentUser);
+
 			// Create and send 2FA code
-			TwoFactorAuthentication twoFactorAuthentication = new TwoFactorAuthentication(currentUser);
+			/*TwoFactorAuthentication twoFactorAuthentication = new TwoFactorAuthentication(currentUser);
 			twoFactorAuthentication.generateCode();
-			twoFactorAuthentication.sendEmail();
+			twoFactorAuthentication.sendEmail();*/
+
 		}
 		else {
 			// Failed login response
