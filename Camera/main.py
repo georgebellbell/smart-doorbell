bdaaaedd22005f3e5c51d9ad3fef40116e38e9de
B9015109 <b9015109@newcastle.ac.uk>
2021-01-08 11:24:41 +0000

    Added extra request to fall inline with new protocol

--- Camera/main.py
+++ Camera/main.py
@@ -40,6 +40,7 @@ def sendImage():
 	output = '{"request":"image","id":"' + PiId + '","data":"' + imageData + '"}\r\n'
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.connect((host, port))
+		s.sendall(bytes('doorbell','utf-8'))
 		s.sendall(bytes(output, 'utf-8'))
 		data = s.recv(1024)
 
