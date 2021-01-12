9b660cda13d90ece068188a1e5d09ed711460cdc
B9015109 <b9015109@newcastle.ac.uk>
2021-01-12 02:15:48 +0000

    Improved clarity of code

--- Camera/Crop.py
+++ Camera/Crop.py
@@ -5,6 +5,7 @@ import sys
 
 class Crop:
 	def __init__(self, imagePath):
+		# Prepare variables
 		self.imagePath = imagePath
 		self.image = cv2.imread(self.imagePath)
 		self.faces = self.identifyFaces(self)
@@ -21,10 +22,12 @@ class Crop:
 		return len(self.faces)
 
 	def identifyFaces(self):
+		# Make the image grayscale to work better for face identification
 		grayscale = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+		# Pre trained machine learning algorithm to detect faces from the opencv library
 		faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+		# Get the coordinates of the faces from the image
 		faces = faceCascade.detectMultiScale(grayscale, scaleFactor=1.3, minNeighbors=3, minSize=(30, 30))
 
-		print(len(faces), faces)
-
+		print("Found", len(faces), "face(s)", faces)
 		return faces
