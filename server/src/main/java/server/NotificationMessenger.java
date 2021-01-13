b0e4ad420bbb1acfbb49ce24a22767f18d01292f
George Bell <b9015566@newcastle.ac.uk>
2021-01-13 17:48:47 +0000

    Implemented server side notifications

---
+++ server/src/main/java/server/NotificationMessenger.java
@@ -0,0 +1,53 @@
+package server;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class NotificationMessenger {
+	public static void sendNotification(String token, String title, String text) throws IOException {
+
+
+
+		FileInputStream serviceAccount =
+				new FileInputStream("key.json");
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.build();
+
+		FirebaseApp.initializeApp(options);
+
+		// This registration token comes from the client FCM SDKs.
+		String registrationToken = "dIOSu3QMSIOy8_G3ZAiPN3:APA91bF1HmL1wx29nruL2xheo9KNGZnjuQPv88RguGNxl5enwrAWtYIBYfdxKbeTxzzg49WCmx0ZFn-Ja9sD8XiqPv2xwBUOhINSjhzz2pssF2c7kKm9-nnfU1hqFMr7r7XX77W7eH5_";
+
+// See documentation on defining a message payload.
+		Message message = Message.builder()
+				.setNotification(com.google.firebase.messaging.Notification.builder()
+						.setTitle(title)
+						.setBody(text)
+						.build())
+				.setToken(token)
+				.build();
+
+// Send a message to the device corresponding to the provided
+// registration token.
+		String response = null;
+		try {
+			response = FirebaseMessaging.getInstance().send(message);
+		} catch (FirebaseMessagingException e) {
+			e.printStackTrace();
+		}
+// Response is a message ID string.
+		System.out.println("Successfully sent message: " + response);
+
+	}
+}
--- server/src/main/java/server/protocol/DoorbellProtocol.java
+++ server/src/main/java/server/protocol/DoorbellProtocol.java
@@ -3,6 +3,7 @@ package server.protocol;
 import database.Data;
 import facialrecognition.FaceSimilarity;
 import org.springframework.security.crypto.codec.Base64;
+import server.NotificationMessenger;
 import server.ResponseHandler;
 
 import java.sql.Blob;
@@ -26,11 +27,16 @@ public class DoorbellProtocol extends Protocol{
 				blobImage.setBytes(1, Image);
 				dataTable.addRecord(new Data(request.getString("id"), blobImage, "Unknown"));
 				dataTable.disconnect();
+				NotificationMessenger.sendNotification("dIOSu3QMSIOy8_G3ZAiPN3:APA91bF1HmL1wx29nruL2xheo9KNGZnjuQPv88RguGNxl5enwrAWtYIBYfdxKbeTxzzg49WCmx0ZFn-Ja9sD8XiqPv2xwBUOhINSjhzz2pssF2c7kKm9-nnfU1hqFMr7r7XX77W7eH5_",
+						"Someone is at your door", "Open app to find out more");
 				System.out.println("Unrecognised face");
 				response.put("response", "success");
 				response.put("message", "Unknown user at the door");
 			}
 			else {
+				NotificationMessenger.sendNotification("dIOSu3QMSIOy8_G3ZAiPN3:APA91bF1HmL1wx29nruL2xheo9KNGZnjuQPv88RguGNxl5enwrAWtYIBYfdxKbeTxzzg49WCmx0ZFn-Ja9sD8XiqPv2xwBUOhINSjhzz2pssF2c7kKm9-nnfU1hqFMr7r7XX77W7eH5_",
+						faceRecognised + " is at the door", "Open app to let them in");
+
 				System.out.println("Recognised face");
 				response.put("response", "success");
 				response.put("message", faceRecognised + " is at the door");
