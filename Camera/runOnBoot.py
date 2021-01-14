4bf61e7e12a5d616450ea5485c7fa8cb8ae6443b
B9015109 <b9015109@newcastle.ac.uk>
2021-01-14 02:35:10 +0000

    Changed cwd of program when executing on boot to fix errors

--- Camera/runOnBoot.py
+++ Camera/runOnBoot.py
@@ -1,11 +1,11 @@
 import os
 import re
 
-# Current working directory
+# Directory of runOnBoot
 cwd = os.getcwd()
-cwd = os.getcwd()
-fileLocation = cwd + "/" + __file__
+fileLocation = (cwd + "/" + __file__)[:-12]
 print(fileLocation)
+
 # Path to file that is to be edited
 path = "/etc/rc.local"
 
@@ -30,7 +30,7 @@ if os.path.exists(path):
 		if line == "exit 0\n":
 			flag = True
 			# Add the script to run the camera on boot
-			output.append("cd " + cwd + " && sudo -u pi python3 " + cwd + "/main.py &\n")
+			output.append("sudo -u pi python3 " + fileLocation + "/main.py &\n")
 			output.append(line)
 
 		elif None != re.search("main\.py", line):
