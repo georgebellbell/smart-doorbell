fc510e0123be1a3a330afcbf43b6b68dc3de8170
B9015109 <b9015109@newcastle.ac.uk>
2021-01-15 11:27:21 +0000

    Rewrote main.py as object oriented and split connection into separate process

--- Camera/main.py
+++ Camera/main.py
@@ -8,113 +8,112 @@ import Crop
 from multiprocessing import Process
 import SocketListener
 
-# Get file path
-cwd = os.getcwd() + "/"
-fileLocation = cwd + __file__[:-7]
-photoPath = fileLocation + "photo.jpg"
 
-# GPIO pin setup
-led1 = LED(10)
-button1 = Button(15)
-led2 = LED(27)
-led2.on()
+class main:
+	def __init__(self):
+		# Get file path
+		self.cwd = os.getcwd() + "/"
+		self.fileLocation = self.cwd + __file__[:-7]
+		self.photoPath = self.fileLocation + "photo.jpg"
 
-button2 = Button(3)
+		# GPIO pin setup
+		self.led1 = LED(10)
+		self.button1 = Button(15)
+		self.led2 = LED(27)
+		self.led2.on()
 
-# Socket Client
-host, port = "192.168.1.123", 4444
+		self.button2 = Button(3)
 
-# Socket Server
-LHost, LPort = "localhost", 4445
+		# Socket Client
+		self.host, self.port = "192.168.1.123", 4444
 
-# Read the Raspberry Pi's unique ID from a file (ID assigned at factory)
-with open(fileLocation + "PiID.txt", "r") as file:
-	PiId = file.readline()
-	print("Unique Device ID: " + PiId)
+		# Read the Raspberry Pi's unique ID from a file (ID assigned at factory)
+		with open(self.fileLocation + "PiID.txt", "r") as file:
+			self.PiId = file.readline()
+			print("Unique Device ID: " + self.PiId)
 
+		# Create socket
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Take picture
-def capture(path):
-	camera = PiCamera()
-	camera.capture(path)
-	camera.close()
+	def main(self):
+		print(os.getpid())
+		# Main loop
+		while True:
+			if self.button1.is_pressed:
+				self.led1.on()
+				# Delay before capture after pressing them button
+				sleep(0.5)
 
-
-# Send the photo to the socket server
-def sendImage(path):
-	imageData = getImage(path)
-	output = '{"request":"image","id":"' + PiId + '","data":"' + imageData + '"}\r\n'
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.connect((host, port))
-		s.sendall(bytes('doorbell\r\n', 'utf-8'))
-		s.sendall(bytes(output, 'utf-8'))
-		data = s.recv(1024)
-
-	print("Received", repr(data))
-	return
-
-
-# Get the photo from the filesystem and return it in the correct format
-def getImage(path):
-	with open(path, "rb") as image:
-		imageData = str(base64.encodebytes(image.read()))[2:-3]
-
-	return imageData
-
-
-def main():
-	print(os.getpid())
-	# Main loop
-	while True:
-		if button1.is_pressed:
-			led1.on()
-			# Delay before capture after pressing them button
-			sleep(0.5)
-
-			# Take a photo
-			try:
-				capture(photoPath)
-				print("Captured photo")
-			except Exception as e:
-				print("Failed to capture photo")
-
-			# Crop the faces from the image
-			faces = Crop.main(photoPath)
-
-			if faces == 0:
-				print("ERROR - No faces found")
-				led1.off()
-			else:
-				print(str(faces) + " Face(s) found")
-
-				# Send faces to the server
+				# Take a photo
 				try:
-					for n in range(faces):
-						sendImage(fileLocation + str(n) + ".jpg")
-						print("Image " + str(n) + " sent to server")
-
-					# Flash led to show picture has finished being sent
-					led1.off()
-					sleep(1)
-					led1.on()
-					# Delay before being able to be ring the doorbell again
-					sleep(2)  # 20
-
+					self.capture()
+					print("Captured photo")
 				except Exception as e:
-					print("Failed to send image to server")
+					print("Failed to capture photo")
 
-				finally:
-					led1.off()
+				# Crop the faces from the image
+				faces = Crop.main(self.photoPath)
 
-		# Close the program if button 2 is pressed
-		if button2.is_pressed:
-			print("Exit")
-			exit()
+				if faces == 0:
+					print("ERROR - No faces found")
+					self.led1.off()
+				else:
+					print(str(faces) + " Face(s) found")
+
+					# Send faces to the server
+					try:
+						for n in range(faces):
+							self.sendImage(n)
+							print("Image " + str(n) + " sent to server")
+
+						# Flash led to show picture has finished being sent
+						self.led1.off()
+						sleep(1)
+						self.led1.on()
+						# Delay before being able to be ring the doorbell again
+						sleep(2)  # 20
+
+					except Exception as e:
+						print("Failed to send image to server")
+
+					finally:
+						self.led1.off()
+
+	# Take picture
+	def capture(self):
+		camera = PiCamera()
+		camera.capture(self.photoPath)
+		camera.close()
+
+	# Send the photo to the socket server
+	def sendImage(self):
+		imageData = self.getImage()
+		output = '{"request":"image","id":"' + self.PiId + '","data":"' + imageData + '"}\r\n'
+
+		p = Process(target=self.socketSend, args=output)
+		p.start()
+
+		return
+
+	# Get the photo from the filesystem and return it in the correct format
+	def getImage(self, n):
+		with open(self.fileLocation + str(n) + ".jpg", "rb") as image:
+			imageData = str(base64.encodebytes(image.read()))[2:-3]
+
+		return imageData
+
+	def socketSend(self, output):
+		self.socket.connect((self.host, self.port))
+		self.socket.sendall(bytes('doorbell\r\n', 'utf-8'))
+		self.socket.sendall(bytes(output, 'utf-8'))
+		data = self.socket.recv(1024)
+		print("Received", repr(data))
+		return data
 
 
 if __name__ == "__main__":
-	p1 = Process(target=SocketListener.runServer, args=(LHost, LPort))
-	p1.start()
+	# p1 = Process(target=SocketListener.runServer, args=(LHost, LPort))
+	# p1.start()
+	doorbell = main()
+	doorbell.main()
 
-	p2 = Process(target=main)
-	p2.start()
