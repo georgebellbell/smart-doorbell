f6067c88cc151f3d752b20405e1834b68cda4f11
b9025633 <b9025633@newcastle.ac.uk>
2021-01-17 17:16:02 +0000

    Added account request tests for admin protocol

--- server/src/test/java/server/protocol/AdminProtocolTest.java
+++ server/src/test/java/server/protocol/AdminProtocolTest.java
@@ -1,7 +1,10 @@
 package server.protocol;
 
+import database.AccountTable;
 import database.User;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,16 +14,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class AdminProtocolTest {
 	private AdminProtocol protocol;
 	private static AdminProtocol loginProtocol;
+	private static User testUser;
+	private static User testAdmin;
 
 	@BeforeAll
 	static void init() {
+		// Create test accounts
+		testUser = new User("testuser", "quicksolutions.doorbell@gmail.com", "password", "user");
+		testAdmin = new User("testadmin", "quicksolutions.doorbell@gmail.com", "password", "admin");
+		saveUser(testUser);
+		saveUser(testAdmin);
+
+		// Create protocol that is logged in as admin
 		loginProtocol = new AdminProtocol();
 		JSONObject request = new JSONObject();
 		request.put("request","login");
-		request.put("username", "admin");
-		request.put("password", "password");
+		request.put("username", testAdmin.getUsername());
+		request.put("password", testAdmin.getPassword());
 		loginProtocol.setRequest(request.toString());
 		loginProtocol.processInput();
+	}
+
+	@AfterAll
+	static void cleanUp() {
+		deleteUser(testUser);
+		deleteUser(testAdmin);
 	}
 
 	@BeforeEach
@@ -28,12 +46,26 @@ class AdminProtocolTest {
 		protocol = new AdminProtocol();
 	}
 
+	static void saveUser(User user) {
+		AccountTable accountTable = new AccountTable();
+		accountTable.connect();
+		accountTable.addRecord(user);
+		accountTable.disconnect();
+	}
+
+	static void deleteUser(User user) {
+		AccountTable accountTable = new AccountTable();
+		accountTable.connect();
+		accountTable.deleteRecord(user.getUsername());
+		accountTable.disconnect();
+	}
+
 	@Test
 	void testValidRequest() {
 		JSONObject request = new JSONObject();
 		request.put("request","login");
-		request.put("username", "admin");
-		request.put("password", "password");
+		request.put("username", testAdmin.getUsername());
+		request.put("password", testAdmin.getPassword());
 
 		assertTrue(protocol.isRequestValid(request.toString()));
 	}
@@ -82,8 +114,8 @@ class AdminProtocolTest {
 	void testLoginCorrectDetails() {
 		JSONObject request = new JSONObject();
 		request.put("request","login");
-		request.put("username", "admin");
-		request.put("password", "password");
+		request.put("username", testAdmin.getUsername());
+		request.put("password", testAdmin.getPassword());
 		protocol.setRequest(request.toString());
 		JSONObject response = new JSONObject(protocol.processInput());
 		assertEquals("success", response.getString("response"));
@@ -93,7 +125,7 @@ class AdminProtocolTest {
 	void testLoginIncorrectDetails() {
 		JSONObject request = new JSONObject();
 		request.put("request","login");
-		request.put("username", "admin");
+		request.put("username", testAdmin.getUsername());
 		request.put("password", "incorrect");
 		protocol.setRequest(request.toString());
 		JSONObject response = new JSONObject(protocol.processInput());
@@ -104,8 +136,8 @@ class AdminProtocolTest {
 	void testLoginNonAdminAccount() {
 		JSONObject request = new JSONObject();
 		request.put("request","login");
-		request.put("username", "jack");
-		request.put("password", "password");
+		request.put("username", testUser.getUsername());
+		request.put("password", testUser.getPassword());
 		protocol.setRequest(request.toString());
 		JSONObject response = new JSONObject(protocol.processInput());
 		assertEquals("fail", response.getString("response"));
@@ -119,4 +151,96 @@ class AdminProtocolTest {
 		JSONObject response = new JSONObject(protocol.processInput());
 		assertEquals("invalid", response.getString("response"));
 	}
+
+	@Test
+	void testSearchForExistingUser() {
+		JSONObject request = new JSONObject();
+		request.put("request","user");
+		request.put("username", testUser.getUsername());
+		loginProtocol.setRequest(request.toString());
+		JSONObject response = new JSONObject(loginProtocol.processInput());
+		assertEquals("success", response.getString("response"));
+	}
+
+	@Test
+	void testSearchForNonExistentUser() {
+		JSONObject request = new JSONObject();
+		request.put("request","user");
+		request.put("username","non-existent-user");
+		loginProtocol.setRequest(request.toString());
+		JSONObject response = new JSONObject(loginProtocol.processInput());
+		assertEquals("fail", response.getString("response"));
+	}
+
+	@Test
+	void testUpdateExistingUser() {
+		JSONObject request = new JSONObject();
+		request.put("request","user");
+		request.put("username", testUser.getUsername());
+		request.put("newusername", testUser.getUsername());
+		request.put("newemail", testUser.getEmail());
+		request.put("devices", new JSONArray());
+		loginProtocol.setRequest(request.toString());
+		JSONObject response = new JSONObject(loginProtocol.processInput());
+		assertAll("success",
+				() -> assertEquals("success", response.getString("response")),
+				() -> assertEquals(testUser.getUsername(), response.getString("username")),
+				() -> assertEquals(testUser.getEmail(), response.getString("email")),
+				() -> assertEquals(testUser.getCreated_at(), response.getString("time"))
+		);
+	}
+
+	@Test
+	void testUpdateExistingUserToAnotherExistingUsername() {
+		JSONObject request = new JSONObject();
+		// Attempt to set testUser's username to testAdmin's username
+		request.put("request","update");
+		request.put("username", testUser.getUsername());
+		request.put("newusername", testAdmin.getUsername());
+		request.put("newemail", testUser.getEmail());
+		request.put("devices", new JSONArray());
+		loginProtocol.setRequest(request.toString());
+		JSONObject response = new JSONObject(loginProtocol.processInput());
+		assertAll("fail",
+				() -> assertEquals("fail", response.getString("response")),
+				() -> assertEquals("Account username is already taken", response.getString("message"))
+		);
+	}
+
+	@Test
+	void testDeleteUser() {
+		User userToDelete = new User("usertodelete", "delete@delete.com",
+				"password", "user");
+		saveUser(userToDelete);
+		JSONObject request = new JSONObject();
+		request.put("request","deleteuser");
+		request.put("username", userToDelete.getUsername());
+		loginProtocol.setRequest(request.toString());
+		JSONObject response = new JSONObject(loginProtocol.processInput());
+		assertEquals("success", response.getString("response"));
+	}
+
+	@Test
+	void testDeleteCurrentAdminAccount() {
+		JSONObject request = new JSONObject();
+		request.put("request","deleteuser");
+		request.put("username", testAdmin.getUsername());
+		loginProtocol.setRequest(request.toString());
+		JSONObject response = new JSONObject(loginProtocol.processInput());
+		assertAll("fail",
+				() -> assertEquals("fail", response.getString("response")),
+				() -> assertEquals("You cannot delete your own account", response.getString("message"))
+		);
+	}
+
+	@Test
+	void testResetUserPassword() {
+		JSONObject request = new JSONObject();
+		request.put("request","newpassword");
+		request.put("username", testUser.getUsername());
+		loginProtocol.setRequest(request.toString());
+		JSONObject response = new JSONObject(loginProtocol.processInput());
+		assertEquals("success", response.getString("response"));
+	}
+
 }
