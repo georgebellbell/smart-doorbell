bdc7f583809af852f395490581a45d99048391c7
B9015109 <b9015109@newcastle.ac.uk>
2021-01-17 19:32:59 +0000

    Updated runOnBoot.py to use object oriented structure

--- Camera/runOnBoot.py
+++ Camera/runOnBoot.py
@@ -2,83 +2,89 @@ import os
 import re
 import sys
 
-# Get arguments
-arguments = sys.argv
-print(arguments, len(arguments))
 
-# Directory of runOnBoot
-cwd = os.getcwd()
-fileLocation = (cwd + "/" + __file__)[:-12]
-print("Directory of Program", fileLocation)
+class runOnBoot:
+	def __init__(self):
+		# Get arguments
+		self.arguments = sys.argv
+		print(self.arguments, len(self.arguments))
 
-# Path to file that is to be edited
-path = "/etc/rc.local"
+		# Directory of runOnBoot
+		self.cwd = os.getcwd()
+		self.fileLocation = (self.cwd + "/" + __file__)[:-12]
+		print("Directory of Program", self.fileLocation)
 
-# Check that the program is run with sudo
-if os.geteuid() != 0:
-	print("Permission denied. Must be run with sudo")
-	exit()
+		# Path to file that is to be edited
+		self.path = "/etc/rc.local"
 
-# Checks that the file exists
-if os.path.exists(path):
-	# Opens file in read mode
-	file = open(path, "r")
+		# Check that the program is run with sudo
+		if os.geteuid() != 0:
+			print("ERROR: Permission denied. Must be run with sudo")
+			exit()
 
-	status = "FAILURE"
-	add = None
+		# Output error if the file doesn't exist
+		if not os.path.exists(self.path):
+			print("ERROR: File not found")
+			exit()
 
-	# Output array
-	output = []
+	def runOnBoot(self):
+		output = self.readFile()
+		self.updateFile(output)
 
-	# Go through every line in the file
-	for line in file:
-		# Check for the end of the file
-		if line == "exit 0\n":
-			if len(arguments) == 2:
-				if str(arguments[1]).lower() == "true":
-					add = True
-				elif str(arguments[1]).lower == "false":
+	def readFile(self):
+		# Opens file in read mode
+		file = open(self.path, "r")
+
+		output = []
+		for line in file:
+			# Check for the end of the file
+			if line == "exit 0\n":
+				if len(self.arguments) == 2:
+					if str(self.arguments[1]).lower() == "true":
+						print("arg - true")
+						add = True
+					elif str(self.arguments[1]).lower == "false":
+						print("arg - false")
+						add = False
+					else:
+						print("arg - unknown")
+						add = False
+
+				elif len(self.arguments) >= 3:
+					print("Too many arguments")
 					add = False
 				else:
-					print("Program takes up to 1 argument. true or false \nAborted")
-					add = False
+					print("arg - none")
+					add = True
 
-			elif len(arguments) >= 3:
-				print("Program takes up to 1 argument. true or false \nAborted")
-				add = False
+				if add:
+					print("Run On Boot ENABLED")
+					# Add the script to run the camera on boot
+					output.append("sudo -u pi python3 " + self.fileLocation + "/main.py &\n")
+					output.append(line)
+				else:
+					print("Run On Boot Disabled")
+					output.append(line)
+
+			elif None != re.search("main\.py", line):
+				pass  # Skips adding the old script to the file
 			else:
-				add = True
-
-			if add:
-				status = "SUCCESS: Run On Boot ENABLED"
-				# Add the script to run the camera on boot
-				output.append("sudo -u pi python3 " + fileLocation + "/main.py &\n")
-				output.append(line)
-			else:
-				status = "SUCCESS: Run On Boot Dissabled"
 				output.append(line)
 
-		elif None != re.search("main\.py", line):
-			pass  # Skips adding the old script to the file
-		else:
-			output.append(line)
-	# Close the file
-	file.close()
+		# Close the file
+		file.close()
 
-	# Open the file in write mode
-	file = open(path, "w")
-	# Overwrite the file with the new contents
-	for line in output:
-		file.write(line)
-	# Close the file
-	file.close()
+		return output
 
-	# Print error if it failed to find exit 0 in the file
-	
-	if status == "FAILURE":
-		print("ERROR: EOF not found")
-	print(status)
-	
-else:
-	# Output error if the file doesn't exist
-	print("ERROR: File not found")
+	def updateFile(self, output):
+		# Open the file in write mode
+		file = open(self.path, "w")
+		# Overwrite the file with the new contents
+		for line in output:
+			file.write(line)
+		# Close the file
+		file.close()
+
+
+if __name__ == "__main__":
+	runOnBoot().runOnBoot()
