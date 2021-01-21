3aeab7a9e276a74a30462a146bf8b52c63d4abf2
b9025633 <b9025633@newcastle.ac.uk>
2021-01-21 16:35:23 +0000

    Added login tests for user protocol

--- server/src/test/java/server/protocol/UserProtocolTest.java
+++ server/src/test/java/server/protocol/UserProtocolTest.java
@@ -1,5 +1,7 @@
 package server.protocol;
 
+import database.AccountTable;
+import database.User;
 import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,14 +11,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserProtocolTest {
 	private Protocol protocol;
+	private User testUser = new User("testuser",
+			"quicksolutions.doorbell@gmail.com", "password", "user");
+	private User testAdmin = new User("testadmin",
+			"quicksolutions.doorbell@gmail.com", "password", "admin");
+	private String testToken = "testToken23443534534556546";
+	private AccountTable accountTable = new AccountTable();
 
 	@BeforeEach
 	void setup() {
 		protocol = new UserProtocol();
+		accountTable.addRecord(testUser);
+		accountTable.deleteRecord(testAdmin);
 	}
 
 	@AfterEach
 	void tearDown() {
+		accountTable.deleteRecord(testUser);
+		accountTable.deleteRecord(testAdmin);
 	}
 
 	@Test
@@ -76,6 +88,42 @@ class UserProtocolTest {
 		request.put("request","login");
 		request.put("username", "Dom");
 		assertFalse(protocol.isRequestValid(request.toString()));
+	}
+
+	@Test
+	void testLoginCorrectDetails() {
+		JSONObject request = new JSONObject();
+		request.put("request","login");
+		request.put("username", testUser.getUsername());
+		request.put("password", testUser.getPassword());
+		request.put("token", testToken);
+		protocol.setRequest(request.toString());
+		JSONObject response = new JSONObject(protocol.processRequest());
+		assertEquals("success", response.getString("response"));
+	}
+
+	@Test
+	void testLoginIncorrectDetails() {
+		JSONObject request = new JSONObject();
+		request.put("request","login");
+		request.put("username", testUser.getUsername());
+		request.put("password", "incorrect");
+		request.put("token", testToken);
+		protocol.setRequest(request.toString());
+		JSONObject response = new JSONObject(protocol.processRequest());
+		assertEquals("fail", response.getString("response"));
+	}
+
+	@Test
+	void testLoginNonUserAccount() {
+		JSONObject request = new JSONObject();
+		request.put("request","login");
+		request.put("username", testAdmin.getUsername());
+		request.put("password", testAdmin.getPassword());
+		request.put("token", testToken);
+		protocol.setRequest(request.toString());
+		JSONObject response = new JSONObject(protocol.processRequest());
+		assertEquals("fail", response.getString("response"));
 	}
 
 }
