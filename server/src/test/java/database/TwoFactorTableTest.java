4fdbec7a91e6964f1bc0e4ebb3d6e618c58e9a0a
Dominykas Makarovas <b9021925@newcastle.ac.uk>
2021-01-21 16:35:22 +0000

    2FA Table tests completed and passed.

--- server/src/test/java/database/TwoFactorTableTest.java
+++ server/src/test/java/database/TwoFactorTableTest.java
@@ -1,5 +1,6 @@
 package database;
 
+import authentication.TwoFactorAuthentication;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,24 +8,37 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TwoFactorTableTest {
+	private TwoFactorTable twoFactorTable;
+	private TwoFactorAuthentication twoFactor;
+	private User user;
+	private String code;
 
 	@BeforeEach
 	void setUp() {
+		twoFactorTable = new TwoFactorTable();
+		user = new User("Test", "quicksolutions.doorbell@gmail.com",
+				"Password", "Salt", "User");
+		code = "123456";
 	}
 
 	@AfterEach
 	void tearDown() {
+		twoFactorTable.deleteRecord(user);
 	}
 
 	@Test
 	void addRecord() {
+		assertTrue(twoFactorTable.addRecord(user, code));
 	}
 
 	@Test
 	void getCode() {
+		addRecord();
+		assertEquals(twoFactorTable.getCode(user), code);
 	}
 
 	@Test
 	void deleteRecord() {
+		assertTrue(twoFactorTable.deleteRecord(user));
 	}
 }
