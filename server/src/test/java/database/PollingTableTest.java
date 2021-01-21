6b62dafdce8f672be7106c26ecba951d3d607a3c
Dominykas Makarovas <b9021925@newcastle.ac.uk>
2021-01-21 16:12:51 +0000

    Implemented PollingTable class tests

--- server/src/test/java/database/PollingTableTest.java
+++ server/src/test/java/database/PollingTableTest.java
@@ -4,27 +4,47 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PollingTableTest {
+	private PollingTable pollingTable;
+	private DoorbellTable doorbellTable;
+	private Doorbell doorbell;
+	private String message;
 
 	@BeforeEach
 	void setUp() {
+		pollingTable = new PollingTable();
+		doorbellTable = new DoorbellTable();
+
+		doorbell = new Doorbell("QS-12345", "TestDoorbell");
+		message = "OPEN DOOR";
+
+		doorbellTable.addNewDoorbell(doorbell);
 	}
 
 	@AfterEach
 	void tearDown() {
+		pollingTable.deletePolls(doorbell.getId());
 	}
 
 	@Test
 	void createPoll() {
+		assertTrue(pollingTable.createPoll(doorbell.getId(), message));
 	}
 
 	@Test
 	void getPolls() {
+		ArrayList<String> messages = pollingTable.getPolls(doorbell.getId());
+		for (int i = 0; i < messages.size(); i++) {
+			assertEquals(messages.get(i), message);
+		}
 	}
 
 	@Test
 	void deletePolls() {
+		assertTrue(pollingTable.deletePolls(doorbell.getId()));
 	}
 }
