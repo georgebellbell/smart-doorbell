4eae491220c8a9dc6e80feabdf5a717991fa8177
George Bell <b9015566@newcastle.ac.uk>
2021-01-24 19:21:22 +0000

    Improved Logout test class where I could so two out of three tests will work all the time, third still requires account to be deleted manually before rerunning

--- app/src/androidTest/java/com/example/doorbellandroidapp/LogoutTest.java
+++ app/src/androidTest/java/com/example/doorbellandroidapp/LogoutTest.java
@@ -13,6 +13,8 @@ import org.junit.runners.JUnit4;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -26,22 +28,18 @@ public class LogoutTest {
 	@Rule
 	public ActivityScenarioRule<SignUpActivity> signUpActivityActivityScenarioRule = new ActivityScenarioRule<>(SignUpActivity.class);
 
-	/**
-	 * Before each test create a new account
-	 */
-	@Before
-	public void setup() throws InterruptedException {
-		TestHelper.accountCreatedSuccessfully();
-	}
 
 	/**
 	 * Test if pressing the logout button creates logout confirm popup
 	 */
 	@Test
-	public void openLogoutConfirmationPopup(){
+	public void openLogoutConfirmationPopup() throws InterruptedException {
+		TestHelper.accountCreatedSuccessfully();
 		onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
 		onView(withId(R.id.navigationView)).perform(NavigationViewActions.navigateTo(R.id.nav_logout));
 		onView(withId(R.id.btnConfirmLogout)).check(matches(isDisplayed()));
+		onView(withId(R.id.btnCancelLogout)).perform(click());
+		TestHelper.deleteAccount();
 	}
 
 
@@ -49,27 +47,38 @@ public class LogoutTest {
 	 * Confirm cancelling logout will get rid of popup
 	 */
 	@Test
-	public void logoutCancel(){
-		openLogoutConfirmationPopup();
+	public void logoutCancel() throws InterruptedException {
+		TestHelper.accountCreatedSuccessfully();
+		onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+		onView(withId(R.id.navigationView)).perform(NavigationViewActions.navigateTo(R.id.nav_logout));
 		onView(withId(R.id.btnCancelLogout)).perform(click());
 		onView(withId(R.id.btnConfirmLogout)).check(doesNotExist());
+		TestHelper.deleteAccount();
 	}
 
 	/**
-	 * After each test delete the account
+	 * Creates account just for testing log out
+	 * Due to limitations of testing, this account will have to be deleted manually before running test again
 	 */
-	@After
-	public void cleanup() throws InterruptedException {
-		TestHelper.deleteAccount();
+	public void createLogOutAccount() throws InterruptedException {
+		onView(withId(R.id.etUsername)).perform(typeText("Logout"), closeSoftKeyboard());
+		onView(withId(R.id.etEmailAddress)).perform(typeText("Logout@gmail.com"), closeSoftKeyboard());
+		onView(withId(R.id.pwdPassword)).perform(typeText("Password123"), closeSoftKeyboard());
+		onView(withId(R.id.btnSignUp)).perform(click());
+		Thread.sleep(3000);
+		onView(withId(R.id.tvLastFace)).check(matches(isDisplayed()));
 	}
+
 
 	/**
 	 * Confirm logging out works as intended and sends user back to login page
 	 * WARNING AFTER RUNNING THIS TEST YOU WILL HAVE TO MANUALLY DELETE ACCOUNT
 	 */
 	@Test
-	public void logoutConfirm(){
-		openLogoutConfirmationPopup();
+	public void logoutConfirm() throws InterruptedException {
+		createLogOutAccount();
+		onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+		onView(withId(R.id.navigationView)).perform(NavigationViewActions.navigateTo(R.id.nav_logout));
 		onView(withId(R.id.btnConfirmLogout)).perform(click());
 		try {
 			Thread.sleep(3000);
