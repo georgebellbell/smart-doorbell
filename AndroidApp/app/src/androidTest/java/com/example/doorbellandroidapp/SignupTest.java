d38c506bbab01dd6ead26bb50f0c415811dc3efc
George Bell <b9015566@newcastle.ac.uk>
2021-01-27 10:28:39 +0000

    Small fixes and fixed some testing on the sign up page as well as added a couple more tests

--- AndroidApp/app/src/androidTest/java/com/example/doorbellandroidapp/SignupTest.java
+++ AndroidApp/app/src/androidTest/java/com/example/doorbellandroidapp/SignupTest.java
@@ -66,6 +66,7 @@ public class SignupTest {
 		onView(withId(R.id.etUsername)).perform(typeText("george"), closeSoftKeyboard());
 		onView(withId(R.id.etEmailAddress)).perform(typeText("TestUser@gmail.com"), closeSoftKeyboard());
 		onView(withId(R.id.pwdPassword)).perform(typeText("Password123"), closeSoftKeyboard());
+		onView(withId(R.id.btnSignUp)).perform(click());
 		Thread.sleep(2000);
 		onView(withId(R.id.tvSignUp)).check(matches(isDisplayed()));
 	}
@@ -78,7 +79,8 @@ public class SignupTest {
 		onView(withId(R.id.etUsername)).perform(typeText("t"), closeSoftKeyboard());
 		onView(withId(R.id.etEmailAddress)).perform(typeText("t@gmail.com"), closeSoftKeyboard());
 		onView(withId(R.id.pwdPassword)).perform(typeText("Password123"), closeSoftKeyboard());
-		onView(withText(R.string.shortUsernameError));
+		onView(withId(R.id.btnSignUp)).perform(click());
+		onView(withText(R.string.shortUsernameError)).check(matches(isDisplayed()));
 		onView(withId(R.id.tvSignUp)).check(matches(isDisplayed()));
 
 	}
@@ -91,7 +93,8 @@ public class SignupTest {
 		onView(withId(R.id.etUsername)).perform(typeText(""), closeSoftKeyboard());
 		onView(withId(R.id.etEmailAddress)).perform(typeText("nothing@gmail.com"), closeSoftKeyboard());
 		onView(withId(R.id.pwdPassword)).perform(typeText("Password123"), closeSoftKeyboard());
-		onView(withText(R.string.noUppercaseError));
+		onView(withId(R.id.btnSignUp)).perform(click());
+		onView(withText(R.string.shortUsernameError)).check(matches(isDisplayed()));
 		onView(withId(R.id.tvSignUp)).check(matches(isDisplayed()));
 	}
 
@@ -103,23 +106,12 @@ public class SignupTest {
 		onView(withId(R.id.etUsername)).perform(typeText("    "), closeSoftKeyboard());
 		onView(withId(R.id.etEmailAddress)).perform(typeText("space@gmail.com"), closeSoftKeyboard());
 		onView(withId(R.id.pwdPassword)).perform(typeText("Password123"), closeSoftKeyboard());
-		onView(withText(R.string.spacesError));
+		onView(withId(R.id.btnSignUp)).perform(click());
+		onView(withText(R.string.spacesError)).check(matches(isDisplayed()));
 		onView(withId(R.id.tvSignUp)).check(matches(isDisplayed()));
 	}
 
 	//EMAIL TESTS
-
-	/**
-	 * Attempting to create an account with an email that is already taken
-	 */
-	@Test
-	public void emailIsNotUnique() throws InterruptedException {
-		onView(withId(R.id.etUsername)).perform(typeText("NotUnique"), closeSoftKeyboard());
-		onView(withId(R.id.etEmailAddress)).perform(typeText("g.bell1@newcastle.ac.uk"), closeSoftKeyboard());
-		onView(withId(R.id.pwdPassword)).perform(typeText("Password123"), closeSoftKeyboard());
-		Thread.sleep(2000);
-		onView(withId(R.id.tvSignUp)).check(matches(isDisplayed()));
-	}
 
 	/**
 	 * Attempting to create an account with an email that isn't in a valid format
@@ -129,7 +121,8 @@ public class SignupTest {
 		onView(withId(R.id.etUsername)).perform(typeText("BadEmail"), closeSoftKeyboard());
 		onView(withId(R.id.etEmailAddress)).perform(typeText("TestUser.com"), closeSoftKeyboard());
 		onView(withId(R.id.pwdPassword)).perform(typeText("Password123"), closeSoftKeyboard());
-		onView(withText(R.string.validEmailError));
+		onView(withId(R.id.btnSignUp)).perform(click());
+		onView(withText(R.string.validEmailError)).check(matches(isDisplayed()));
 		onView(withId(R.id.tvSignUp)).check(matches(isDisplayed()));
 	}
 
@@ -142,7 +135,8 @@ public class SignupTest {
 		onView(withId(R.id.etUsername)).perform(typeText("shortPassword"), closeSoftKeyboard());
 		onView(withId(R.id.etEmailAddress)).perform(typeText("shortPassword@gmail.com"), closeSoftKeyboard());
 		onView(withId(R.id.pwdPassword)).perform(typeText("Pass1"), closeSoftKeyboard());
-		onView(withText(R.string.shortPasswordError));
+		onView(withId(R.id.btnSignUp)).perform(click());
+		onView(withText(R.string.shortPasswordError)).check(matches(isDisplayed()));
 		onView(withId(R.id.tvSignUp)).check(matches(isDisplayed()));
 
 	}
@@ -155,7 +149,8 @@ public class SignupTest {
 		onView(withId(R.id.etUsername)).perform(typeText("spacePassword"), closeSoftKeyboard());
 		onView(withId(R.id.etEmailAddress)).perform(typeText("spacePassword@gmail.com"), closeSoftKeyboard());
 		onView(withId(R.id.pwdPassword)).perform(typeText("Pass word 123"), closeSoftKeyboard());
-		onView(withText(R.string.spacesError));
+		onView(withId(R.id.btnSignUp)).perform(click());
+		onView(withText(R.string.spacesError)).check(matches(isDisplayed()));
 		onView(withId(R.id.tvSignUp)).check(matches(isDisplayed()));
 	}
 
@@ -167,7 +162,8 @@ public class SignupTest {
 		onView(withId(R.id.etUsername)).perform(typeText("uppercasePassword"), closeSoftKeyboard());
 		onView(withId(R.id.etEmailAddress)).perform(typeText("cap@gmail.com"), closeSoftKeyboard());
 		onView(withId(R.id.pwdPassword)).perform(typeText("PASSWORD123"), closeSoftKeyboard());
-		onView(withText(R.string.noLowercaseError));
+		onView(withId(R.id.btnSignUp)).perform(click());
+		onView(withText(R.string.noLowercaseError)).check(matches(isDisplayed()));
 		onView(withId(R.id.tvSignUp)).check(matches(isDisplayed()));
 	}
 
@@ -179,7 +175,22 @@ public class SignupTest {
 		onView(withId(R.id.etUsername)).perform(typeText("lowercasePassword"), closeSoftKeyboard());
 		onView(withId(R.id.etEmailAddress)).perform(typeText("nocap@gmail.com"), closeSoftKeyboard());
 		onView(withId(R.id.pwdPassword)).perform(typeText("password123"), closeSoftKeyboard());
-		onView(withText(R.string.noUppercaseError));
+		onView(withId(R.id.btnSignUp)).perform(click());
+		onView(withText(R.string.noUppercaseError)).check(matches(isDisplayed()));
 		onView(withId(R.id.tvSignUp)).check(matches(isDisplayed()));
+	}
+
+	/**
+	 * Attempting to create an account with a password with no numbers
+	 */
+	@Test
+	public void passwordWithNoNumbers() throws InterruptedException {
+		onView(withId(R.id.etUsername)).perform(typeText("nonumber"), closeSoftKeyboard());
+		onView(withId(R.id.etEmailAddress)).perform(typeText("nonumber@gmail.com"), closeSoftKeyboard());
+		onView(withId(R.id.pwdPassword)).perform(typeText("Passwords"), closeSoftKeyboard());
+		onView(withId(R.id.btnSignUp)).perform(click());
+		onView(withText(R.string.noNumberInPasswordError)).check(matches(isDisplayed()));
+		onView(withId(R.id.tvSignUp)).check(matches(isDisplayed()));
+		Thread.sleep(3000);
 	}
 }
